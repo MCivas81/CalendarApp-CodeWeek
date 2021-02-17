c82958a770b221d1695fb46aec49c2538aa264ca
Marco Civello <marco.civello@gmail.com>
2021-02-17 20:20:31 +0100

    I've added all the features as requested by the client

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,28 @@
-const types = [{
-  id: 1,
-  name: 'Parrucchieria'
-},
-{
-  id: 2,
-  name: 'Manicure'
-},
-{
-  id: 3,
-  name: 'Altro'
-}
+const types = [
+  {
+    id: 1,
+    name: 'Parrucchieria'
+  },
+  {
+    id: 2,
+    name: 'Manicure'
+  },
+  {
+    id: 3,
+    name: 'Altro'
+  }
 ];
 
 let hairdresserApp = [];
 let manicureApp = [];
 let otherApp = [];
-console.log(hairdresserApp);
-console.log(manicureApp);
-console.log(otherApp);
+
+const cardWrapperHair = document.querySelector('#wrapperHair');
+const cardWrapperMani = document.querySelector('#wrapperMani');
+const cardWrapperOther = document.querySelector('#wrapperOther');
+const descriptionHair = document.querySelector('#description-hair');
+const descriptionMani = document.querySelector('#description-mani');
+const descriptionOther = document.querySelector('#description-other');
 
 // Function that generates a random number between min e max
 function getRandomInt(min, max) {
@@ -25,6 +30,36 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //max included - min included
 };
+
+const renderFunction = (renderList, appendElementId) => {
+  renderList.forEach(appointment => {
+    const myPriority = document.createElement('p')
+    const myCardText = document.createElement('p');
+    const mytypeId = document.createElement('h2');
+    const myCardBody = document.createElement('div');
+    const myCardHeader = document.createElement('div');
+    const myCard = document.createElement('div');
+
+    myPriority.textContent = 'Priorità: ' + appointment.priority;
+    myCardText.textContent = appointment.title;
+    mytypeId.textContent = 'Cliente: ' + appointment.id;
+
+    myPriority.classList.add('priority');
+    myCardBody.classList.add('card-body');
+    myCardHeader.classList.add('card-header');
+    myCard.classList.add('card');
+
+    if (appointment.completed === true) {
+      myCardBody.classList.add('is-completed');
+      myPriority.textContent = 'Completato'
+    };
+
+    myCardBody.append(myCardText, myPriority);
+    myCardHeader.appendChild(mytypeId);
+    myCard.append(myCardHeader, myCardBody);
+    appendElementId.appendChild(myCard);
+  });
+}
 
 // API call
 async function getAppointmentsAPI() {
@@ -54,8 +89,17 @@ async function getAppointmentsAPI() {
       } else if (newAppointments[i].typeId === types[2].id) {
         otherApp.push(newAppointments[i])
       }
-      
+
     }
+
+    descriptionHair.textContent = 'Appuntamenti Parruccheria: ' + (hairdresserApp.length);
+    descriptionMani.textContent = 'Appuntamenti Manicure: ' + (manicureApp.length);
+    descriptionOther.textContent = 'Altri Appuntamenti: ' + (otherApp.length);
+
+    renderFunction(hairdresserApp, cardWrapperHair);
+    renderFunction(manicureApp, cardWrapperMani);
+    renderFunction(otherApp, cardWrapperOther);
+
     return newAppointments;
 
   } catch (e) {
